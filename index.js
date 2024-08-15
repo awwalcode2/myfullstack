408210eec6e6a1456447481178dca7b16cbef1b2
awwalcode2 <awwalojewale@gmail.com>
2024-08-15 12:21:26 +0100

    second-backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 const connectdb = require("./configs/configs.user")
 const app = express()
 const router = require("./routes/user.router")
+const socket = require("socket.io")
 const cors = require("cors")
 app.use(express.json({extended:true,limit:"50mb"}))
 app.use(cors({origin:"*"}))
@@ -18,15 +19,25 @@ app.use("/user", router)
 // const connectdb = require("./configs/configs.user")
 
 
-
+connectdb()
 
 
 let Port = 4307 || process.env.Port
-app.listen(Port, (err)=>{
+const connection =  app.listen(Port, (err)=>{
     console.log(`app running on ${Port}`);
 })
+const io = socket(connection,{
+    cors:({origin:"*"})
+})
 
-connectdb()
+io.on("connection",(socket)=>{
+    console.log("a user connected successfully");
+    socket.on("newmessage",(message)=>{
+        console.log(message);
+        socket.emit("receivemessage",message)
+    })
+    
+})
 
 
 
